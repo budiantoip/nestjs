2ce653ded82b8a7f94fbba54abb4f439c16cc210
Budianto IP <budiantoip@gmail.com>
2023-08-22 10:24:29 +0700

    add an environment variable and allow all incoming IPv4 connections

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  // This line is important to ensure your app listens to the PORT env var
+  await app.listen(process.env.PORT ?? 3000, "0.0.0.0");
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
